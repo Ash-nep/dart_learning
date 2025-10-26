6ebe01469e93c7f2185aed3c5b647a5f199bbde7
Binod Bhandari <binodcoder@gmail.com>
2025-10-26 09:38:43 +0000

    inheretance demo

--- demo1.dart
+++ demo1.dart
@@ -4,6 +4,10 @@
 void main() {
   Shape shape = Shape("Red");
   shape.displayColor();
+
+  Circle circle = Circle("Green", 2.1);
+  circle.calculateArea();
+  circle.displayColor();
 }
 
 class Shape {
@@ -15,6 +19,22 @@ class Shape {
 
   //methods
   void displayColor() {
-    print(color);
+    print("Color of Shape $color");
+  }
+}
+
+//inheretance
+class Circle extends Shape {
+  //properties
+  double radius;
+  static const double pi = 2.14;
+
+  //constructors
+  Circle(super.color, this.radius);
+
+  //methods
+  void calculateArea() {
+    double area = (pi * radius * radius);
+    print("Area of circle $area");
   }
 }
