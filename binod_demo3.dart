e88255e9091fb647ac415ab074ccdfa3367153ef
Binod Bhandari <binodcoder@gmail.com>
2025-12-28 16:34:17 +0000

    inheritance demo shape

---
+++ binod_demo3.dart
@@ -0,0 +1,28 @@
+class Shape {
+  const Shape(this.color);
+  final String? color;
+}
+
+class Circle extends Shape {
+  const Circle(this.radius) : super('green');
+
+  final double? radius;
+}
+
+class Rectangle extends Shape {
+  const Rectangle(this.length, this.breadth) : super('grey');
+
+  final double? length;
+  final double? breadth;
+}
+
+void main() {
+  Shape s1 = Circle(2.4);
+  print(s1.color);
+
+  Circle c1 = Circle(2.7);
+  print(c1.radius);
+
+  Shape s2 = Rectangle(8.7, 8.4);
+  print(s2.color);
+}
