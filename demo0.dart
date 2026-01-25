f97d58c84579a741604bae834ce10b16d41a54a9
Ash-nep <asharma.it.nepal@gmail.com>
2026-01-25 07:10:05 +0545

    simple interest

--- demo0.dart
+++ demo0.dart
@@ -1,2 +1,15 @@
 // todo : make a function that calculates Simple Interest and return value.
 // todo: that function needs Rate, Time, Principle
+import 'dart:ffi';
+
+void main() {
+  double SI = calculateSimpleInterest(5, 3, 10000);
+  print(SI);
+}
+
+double calculateSimpleInterest(double rate, double time, double principal) {
+  double I = (principal * rate * time);
+  double? interest;
+  interest = I / 100;
+  return interest;
+}
