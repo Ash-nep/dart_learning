fd703fb9fe583e3671ff5b87104ce5b46562f591
Ash-nep <asharma.it.nepal@gmail.com>
2026-01-31 19:57:29 +0545

    greater number among 3

---
+++ demo8.dart
@@ -0,0 +1,16 @@
+void main() {
+  int num1 = 10;
+  int num2 = 5;
+  int num3 = 15;
+  checkGreaterNumber(num1, num2, num3);
+}
+
+int? checkGreaterNumber(int num1, int num2, int num3) {
+  if (num1 > num2 && num1 > num3) {
+    return num1;
+  } else if (num2 > num1 && num2 > num3) {
+    return num2;
+  } else {
+    return num3;
+  }
+}
