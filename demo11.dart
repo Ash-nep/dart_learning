5aec4de0b6cc1476efc5e13c246b41cdb161f95c
Ash-nep <asharma.it.nepal@gmail.com>
2026-03-17 13:21:50 +0545

    circle

--- demo11.dart
+++ demo11.dart
@@ -1,6 +1,14 @@
 
 
 void main(){
+  runApp (Const MaterialApp( home: Scalfold(body : Text ('Hello World!'))) )
+Shape s1 = Circle(2.4);
+print(s1.color);
+
+Circle c1 = Circle(3.0);
+print(c1.radius);
+Shape s2 = Rectangle(8.7, 8.2);
+print(s2.color);
 
 }
 
@@ -12,4 +20,17 @@ num1 + num2;
 }
 void demo(){
   add(a: 5, b: 3);
+}
+class Shape{
+  const Shape(this.color);
+  final String? color;
+}
+class Circle extends Shape {
+  const Circle(this.radius) : super('green');
+final double?radius;
+}
+class Rectangle extends Shape{
+  const Rectangle(this.length, this.breadth) : super('grey');
+  final double? length;
+  final double? breadth;
 }
