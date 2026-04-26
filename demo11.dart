54b5fdf874f77db34aea4168f95741f249cb296f
Ash-nep <asharma.it.nepal@gmail.com>
2026-04-26 17:56:19 +0545

    merge

--- demo11.dart
+++ demo11.dart
@@ -1,22 +1,36 @@
-void main(){
-Shape s1 = Circle(2.4);
-print(s1.color);
-Circle c1 = Circle(3.0);
-print(c1.radius);
-Shape s2 = Rectangle(8.7, 8.2);
-print(s2.color);
+void main() {
+  Shape s1 = Circle(2.4);
+  print(s1.color);
 
+  Circle c1 = Circle(3.0);
+  print(c1.radius);
+  Shape s2 = Rectangle(8.7, 8.2);
+  print(s2.color);
 }
 
-class Shape{
+void add({required a, required b}) {
+  print(a + b);
+}
+
+void addNumbers(num1, num2) {
+  num1 + num2;
+}
+
+void demo() {
+  add(a: 5, b: 3);
+}
+
+class Shape {
   const Shape(this.color);
   final String? color;
 }
+
 class Circle extends Shape {
   const Circle(this.radius) : super('green');
-final double?radius;
+  final double? radius;
 }
-class Rectangle extends Shape{
+
+class Rectangle extends Shape {
   const Rectangle(this.length, this.breadth) : super('grey');
   final double? length;
   final double? breadth;
