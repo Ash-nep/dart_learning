48e7f9b11f899ba5564bfd47b7dff390a53e9fa5
Binod Bhandari <binodcoder@gmail.com>
2026-04-26 13:14:23 +0100

    saved datatype

---
+++ datatypes.dart
@@ -0,0 +1,16 @@
+// data types.
+//todo: find the types of datatpes used in dart, list
+//todo: study about it and do google.
+//todo:watch youtube videos about data types in dart.
+
+
+data: 5, 'ram', 4.56, 5.555555555, 0, 1, 'a'
+num: 4, 566.56, numbers , 
+int: 4, 56, 34, 67,
+float: 3.45, 4.56, 
+double: 2.333334445566, 
+String: "name ofsdfl djfj", address, name, code,
+char: 't', 'd' 
+bit: 0, 1
+var: 
+
